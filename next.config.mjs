c7bd2895e0933342f28002e2417edeef169d3e08
Taofiq Sulayman <sulaymantaofiq@gmail.com>
2024-03-12 12:28:05 +0100

    feat: implement pwa functionality

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
+import { withPWA } from "next-pwa";
+
 const nextConfig = {};
 
-export default nextConfig;
+export default withPWA({
+    ...nextConfig,
+    pwa: {
+        dest: "public",
+        // disable: process.env.NODE_ENV === 'development',
+        register: true,
+        skipWaiting: true,
+        disableDevLogs: true,
+    },
+});

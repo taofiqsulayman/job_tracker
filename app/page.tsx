29582670c55cee77597f749e3cfe0fb76f9af4f7
Taofiq Sulayman <sulaymantaofiq@gmail.com>
2024-03-13 03:51:28 +0100

    fix type and linting issues

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,10 @@ import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { auth } from "@/app/firebase/config";
 import JobCounter from "./components/JobCounter";
+import { User } from "firebase/auth";
 
 export default function Home() {
-    const [user, setUSer] = useState({});
+    const [user, setUSer] = useState<User | null>(null);
     const router = useRouter();
 
     useEffect(() => {
@@ -25,7 +26,7 @@ export default function Home() {
         <main className="flex min-h-screen flex-col items-center p-24 gap-10">
             <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
                 <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-                    Welcome {user.displayName}
+                Welcome {user?.displayName}
                 </p>
                 <button
                     onClick={() => auth.signOut()}
@@ -36,7 +37,7 @@ export default function Home() {
             </div>
 
             <div className="flex flex-col justify-center align-middle w-full">
-                <JobCounter user={user.displayName} />
+                <JobCounter user={user?.displayName || ""} />
             </div>
         </main>
     );

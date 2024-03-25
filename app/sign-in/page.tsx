f2b656b45327f6be46a224f9f42c09b65a1a1621
Taofiq Sulayman <sulaymantaofiq@gmail.com>
2024-03-26 00:21:00 +0100

    Update sign-in page layout and components

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -4,6 +4,8 @@ import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { auth, provider } from "@/app/firebase/config";
 import { useRouter } from "next/navigation";
 import { signInWithPopup } from "firebase/auth";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 const SignIn = () => {
     const [email, setEmail] = useState("");
@@ -35,40 +37,40 @@ const SignIn = () => {
     };
 
     return (
-        <div className="min-h-screen flex flex-col items-center justify-center bg-gray-900">
-            <h1 className="text-white text-3xl mb-5">Job Tracker</h1>
-            <div className="bg-gray-800 p-10 rounded-lg shadow-xl w-96">
-                <h1 className="text-white text-2xl mb-5">Sign In</h1>
-                <input
+        <div className="min-h-screen flex flex-col items-center justify-center">
+            <h1 className="text-3xl mb-5 font-bold">Job Tracker</h1>
+            <div className="flex flex-col gap-5 p-10 rounded-lg shadow-xl w-96">
+                <h1 className="text-2xl font-semibold">Sign In</h1>
+                <Input
                     type="email"
                     placeholder="Email"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
-                    className="w-full p-3 mb-4 bg-gray-700 rounded outline-none text-white placeholder-gray-500"
                 />
-                <input
+
+                <Input
                     type="password"
                     placeholder="Password"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
-                    className="w-full p-3 mb-4 bg-gray-700 rounded outline-none text-white placeholder-gray-500"
                 />
-                <button
-                    onClick={handleSignIn}
-                    className="w-full p-3 bg-indigo-600 rounded text-white hover:bg-indigo-500"
-                >
-                    Sign In
-                </button>
-                <button
-                    onClick={handleGoogleSignIn}
-                    className="w-full p-3 bg-red-600 rounded text-white hover:bg-red-500 mt-4"
-                >
-                    Sign In with Google
-                </button>
-                <p className="text-white mt-4"> {`Don't have an account?`}
+
+                <Button className="w-full p-5" onClick={handleSignIn}>
+                    {" "}
+                    Sign In{" "}
+                </Button>
+
+                <Button className="w-full p-5 bg-sky-700 hover:bg-sky-300" onClick={handleGoogleSignIn}>
+                    {" "}
+                    Sign In with Google{" "}
+                </Button>
+
+                <p className="font-semibold">
+                    {" "}
+                    {`Don't have an account?`}
                     <span
-                        onClick={() => router.push('/sign-up')}
-                        className="text-blue-500 hover:text-blue-400 cursor-pointer ml-2"
+                        onClick={() => router.push("/sign-up")}
+                        className="cursor-pointer ml-2 font-bold hover:underline"
                     >
                         Sign Up
                     </span>
